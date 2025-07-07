1d625840bd48ed57df1193de67b5e6390627a2e9
Oleksandr Dmytriev <sanderdsukr@gmail.com>
2025-07-07 21:28:26 +0300

    color-body-fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,23 @@
-const inputEl = document.querySelector("#name-input");
-const nameOutputEl = document.querySelector("#name-output");
-const titleEl = document.querySelector("h1");
+const refs = {
+  inputEl: document.querySelector("#name-input"),
+  nameOutputEl: document.querySelector("#name-output"),
+  titleEl: document.querySelector("h1"),
+};
 
 const onInput = (e) => {
   const input = e.currentTarget;
 
   if (input.value.trim().length > 0) {
-    nameOutputEl.textContent = input.value.trim();
+    refs.nameOutputEl.textContent = input.value.trim();
   } else {
-    nameOutputEl.textContent = "Anonymous";
+    refs.nameOutputEl.textContent = "Anonymous";
   }
 };
 
-inputEl.addEventListener("input", onInput);
+refs.inputEl.addEventListener("input", onInput);
 
 //Styling
-Object.assign(inputEl.style, {
+Object.assign(refs.inputEl.style, {
   font: "400 16px / 1.5 'Montserrat', sans-serif",
   letterSpacing: "0.04em",
   color: "#2e2f42",
@@ -25,7 +27,7 @@ Object.assign(inputEl.style, {
   borderRadius: "4px",
 });
 
-Object.assign(titleEl.style, {
+Object.assign(refs.titleEl.style, {
   font: "600 24px/1.33333 'Montserrat', sans-serif",
   letterSpacing: "0.04em",
   color: "#2e2f42",
--- js/task-5.js
+++ js/task-5.js
@@ -4,38 +4,42 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
-const bodyEl = document.querySelector("body");
-const btnEl = document.querySelector(".change-color");
-const spanEl = document.querySelector(".color");
-
-const onClick = (e) => {
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  spanEl.textContent = getRandomHexColor();
+const refs = {
+  bodyEl: document.querySelector("body"),
+  btnEl: document.querySelector(".change-color"),
+  spanEl: document.querySelector(".color"),
+  widgetEl: document.querySelector(".widget"),
+  textEl: document.querySelector(".widget > p"),
 };
 
-btnEl.addEventListener("click", onClick);
+const onClick = (e) => {
+  const randColor = getRandomHexColor();
+
+  refs.bodyEl.style.backgroundColor = randColor;
+  refs.spanEl.textContent = randColor;
+};
+
+refs.btnEl.addEventListener("click", onClick);
 
 // Widget Styling
-const widgetEl = document.querySelector(".widget");
 
-Object.assign(widgetEl.style, {
+Object.assign(refs.widgetEl.style, {
   display: "flex",
   flexDirection: "column",
   width: "345px",
   alignItems: "center",
+  padding: "100px 88px",
 });
 
 // Text Styling
-const textEl = document.querySelector(".widget > p");
-
-Object.assign(textEl.style, {
+Object.assign(refs.textEl.style, {
   font: "400 16px / 1.5 'Montserrat', sans-serif",
   letterSpacing: "0.04em",
   color: "#2e2f42",
 });
 
 // Button Styling
-Object.assign(btnEl.style, {
+Object.assign(refs.btnEl.style, {
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
